df5428accb814a8fbd074414851cb51e5cb8bb10
sam tebs <samtebs07@gmail.com>
2025-04-29 05:40:58 +0100

    App Finally Cleared

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -28,78 +28,44 @@ const LoginPage = () => {
         if (res?.success) {
             navigate("/user")
         }
-        // console.log(redirect);
 
-        // navigate('/user'); console.log("c'est moi zeee bg");
-
-        // setLoginLoad(true)
-
-        /*
-        try {
-            const response = await axios.post(`${reqAPI}/login`, LoginForm)
-
-            console.log(LoginForm);
-            console.log(response);
-            if (response.data.status === 'success') {
-                toast.success(response.data.message)
-                localStorage.setItem("user_token", response.data.user_token);
-                console.log(response.data.user_token);
-                setIsAuthenticated(false);
-                setTimeout(() => {
-                    navigate('/user')
-                }, 3000);
-            } else {
-                response.data.message && toast.error(response.data.message)
-                response.data.errors && setLoginErrors(response.data.errors)
-            }
-        } catch (error) {
-            console.log(error);
-            toast.error('Erreur de connexion')
-        } finally {
-            setTimeout(() => {
-                setLoginLoad(false)
-            }, 1000);
-        }
-        */
+        return (
+            <div className="ms_Main">
+                {/* <ToastContainer position="bottom-right" autoClose={1000} /> */}
+                <div className="Main">
+                    <form className="w-8/12 text-gray-600 insc animO flex justify-center flex-wrap h-full items-center" onSubmit={submitLogin}>
+                        <div className="conn w-9/12">
+                            <InputField
+                                label="Numéro de téléphone :"
+                                type="number"
+                                name="number"
+                                onChange={HandleChange}
+                                error={loginErrors?.number}
+                                placeholder="+237 XXXXXXXXX"
+                            />
+                            <InputField
+                                label="Mot de Passe :"
+                                type="password"
+                                name="password"
+                                onChange={HandleChange}
+                                error={loginErrors?.password}
+                                placeholder="Votre mot de passe"
+                            />
+                        </div>
+                        <div className="text-gray-500"></div>
+                        <div className="w-full">
+                            <button type="submit" className={`btn animZ ${isLoading && " w-60 text-center bg-gray-400 text-white cursor-not-allowed"}`} disabled={isLoading}>
+                                {isLoading ? '...' : 'Connexion'}
+                            </button>
+                        </div>
+                        <div className="w-full mt-4">
+                            Vous n'avez pas encore de compte ?<br />
+                            <Link to="/register" className="text-purple-900 hover:underline">Inscrivez-vous !</Link>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        )
     }
 
-    return (
-        <div className="ms_Main">
-            {/* <ToastContainer position="bottom-right" autoClose={1000} /> */}
-            <div className="Main">
-                <form className="w-8/12 text-gray-600 insc animO flex justify-center flex-wrap h-full items-center" onSubmit={submitLogin}>
-                    <div className="conn w-9/12">
-                        <InputField
-                            label="Numéro de téléphone :"
-                            type="number"
-                            name="number"
-                            onChange={HandleChange}
-                            error={loginErrors?.number}
-                            placeholder="+237 XXXXXXXXX"
-                        />
-                        <InputField
-                            label="Mot de Passe :"
-                            type="password"
-                            name="password"
-                            onChange={HandleChange}
-                            error={loginErrors?.password}
-                            placeholder="Votre mot de passe"
-                        />
-                    </div>
-                    <div className="text-gray-500"></div>
-                    <div className="w-full">
-                        <button type="submit" className={`btn animZ ${isLoading && " w-60 text-center bg-gray-400 text-white cursor-not-allowed"}`} disabled={isLoading}>
-                            {isLoading ? '...' : 'Connexion'}
-                        </button>
-                    </div>
-                    <div className="w-full mt-4">
-                        Vous n'avez pas encore de compte ?<br />
-                        <Link to="/register" className="text-purple-900 hover:underline">Inscrivez-vous !</Link>
-                    </div>
-                </form>
-            </div>
-        </div>
-    )
-}
-
-export default LoginPage
+    export default LoginPage
