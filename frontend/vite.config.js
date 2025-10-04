811bbec912cb5d3422d04d37176f8e11661cb067
Teubissi Samuel <samtebs07@gmail.com>
2025-10-04 14:35:58 +0100

    Resolve motion-utils

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: '/',
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
+      '@': path.resolve(__dirname, 'src'),
+    },
   },
   build: {
     chunkSizeWarningLimit: 1000,
@@ -17,10 +17,13 @@ export default defineConfig({
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            return id.split('node_modules/')[1].split('/')[0];
+            return id.split('node_modules/')[1].split('/')[0]
           }
-        }
-      }
-    }
-  }
+        },
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ['framer-motion', 'motion-utils'],
+  },
 })
